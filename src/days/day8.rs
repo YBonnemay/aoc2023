5bbca5b7705db427717515df0d6f8ad8c1ba8d72
Yann Bonnemay <y.bonnemay@gmail.com>
2024-01-02 11:54:46 +0100

    day8

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -7,26 +7,60 @@ struct Direction {
     right: String,
 }
 
-fn process_instructions(instructions: &String, maps: &HashMap<String, Direction>) -> i32 {
+#[derive(Debug, Clone)]
+struct State {
+    idx: i64,
+    value: String,
+}
+
+fn process_instructions(
+    instructions: &str,
+    maps: &HashMap<String, Direction>,
+    state: &mut State,
+) -> State {
     let instructions_chars: Vec<_> = instructions.chars().collect();
     let loop_size = instructions_chars.len();
     let mut zzz_found = false;
-    let mut current_idx = 0;
-    let mut current_value = "AAA".to_string();
+    let mut next_value = "".to_string();
+    let State {
+        idx: mut current_idx,
+        value: ref mut current_value,
+    } = state;
+
     while !zzz_found {
-        let next_value = match instructions_chars[current_idx % loop_size] {
-            'L' => &maps[&current_value].left,
-            'R' => &maps[&current_value].right,
+        next_value = match instructions_chars[(current_idx % loop_size as i64) as usize] {
+            'L' => maps[current_value].left.clone(),
+            'R' => maps[current_value].right.clone(),
             _ => panic!("Err no instruction given"),
         };
-        zzz_found = next_value == "ZZZ";
-        current_value = (*next_value).clone();
+        zzz_found = next_value.ends_with('Z');
+        *current_value = next_value.clone();
         current_idx += 1;
     }
-    current_idx as i32
+
+    State {
+        idx: current_idx,
+        value: next_value,
+    }
 }
 
-fn day8(input: &str) -> i32 {
+fn gcd_of_two_numbers(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
+}
+
+pub fn lcm(nums: &[i64]) -> i64 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn day8(input: &str) -> i64 {
     let mut data = input_to_lines(input);
     let instructions = &data.remove(0);
     data.remove(0);
@@ -44,10 +78,24 @@ fn day8(input: &str) -> i32 {
         );
     }
 
-    process_instructions(instructions, &maps)
+    // Nodes that end with an A
+    let states = maps.iter().fold(Vec::new(), |mut acc, (k, _v)| {
+        if k.ends_with('A') {
+            let mut state = State {
+                idx: 0,
+                value: k.clone(),
+            };
+            let new_state = process_instructions(instructions, &maps, &mut state);
+            acc.push(new_state.idx);
+        }
+        acc
+    });
+
+    lcm(&states)
 }
 
 pub fn run() {
+    // Did not like this one, the description misleads.
     let input = "./days/day8/input.txt";
     let result = day8(input);
     println!("\n day8 done with result_part_one {result} ");
